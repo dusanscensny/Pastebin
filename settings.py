0ffabd49297540ecc04d6431b52b680a7183dcd3
Dusan <dusan.scensny@gmail.com>
2011-06-01 18:48:35 +0200

    Changing html source

--- settings.py
+++ settings.py
@@ -91,3 +91,12 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
+
+# Absolute path to the directory that holds media.
+# Example: "/home/media/media.lawrence.com/"
+MEDIA_ROOT = './static/'
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash if there is a path component (optional in other cases).
+# Examples: "http://media.lawrence.com", "http://example.com/media/"
+MEDIA_URL = 'static/'
