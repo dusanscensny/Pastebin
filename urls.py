b03c8a33286f7f7f60be585325bff8877e68b3a8
Dusan <dusan.scensny@gmail.com>
2011-06-01 17:55:52 +0200

    Basic files to project

---
+++ urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls.defaults import *
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Example:
+    (r'^pastebin/([A-Za-z]+)','pastebin.pasteapp.views.text_by_user'),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # (r'^admin/', include(admin.site.urls)),
+)
