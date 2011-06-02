c6f242facb09aa83299494b0a7e23b98c67d763b
Dusan <dusan.scensny@gmail.com>
2011-06-02 15:25:57 +0200

    creating pasteForms

--- pasteapp/views.py
+++ pasteapp/views.py
@@ -9,3 +9,16 @@ def text_by_user(request, text):
 
 def index(request):
 	return render_to_response('index.html')
+	
+	
+def pasteForm(request):
+	if request.method = 'POST': #form has been submitted
+		form = pasteForm(request.POST) #store POST data
+		if form.is_valid(): #Validation rules
+			
+			return HttpResponseRedirect('/thanks/') #message after post
+	else:
+		form = pasteForm()
+		
+	return render_to_response('contact.html',{'form':form})
+		
