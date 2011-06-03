00d9988a0362ed6a1f632d8ebd5f131eaedec45f
Dusan <dusan.scensny@gmail.com>
2011-06-03 10:45:30 +0200

    generating form by python

--- pasteapp/views.py
+++ pasteapp/views.py
@@ -10,7 +10,23 @@ def text_by_user(request, text):
 	
 
 def index(request):
-	return render_to_response('index.html')
+	if request.method == 'POST': #form has been submitted
+		form = PasteForm(request.POST) #store POST data
+		if form.is_valid(): #Validation rules
+			nick = form.cleaned_data['nick']
+			title = form.cleaned_data['title']
+			content = form.cleaned_data['content']
+			expiration = form.clenaed_data['expiration']
+			datetime = datetime.datetime.now()
+			expiredate = datetime.datetime.now() + datetime.timedelta(seconds=int(expiration))
+			
+			model = Pastebin(nick, datetime, title, content, expiredate)
+			model.save
+			
+			return HttpResponseRedirect('/' + nick + '/') #message after post
+	else:
+		form = PasteForm()	
+	return render_to_response('index.html',{'form':form})
 	
 
 def paste(request):
